909fe1a1213f2543fc1218834806a3149168bb3d
sampayorafael12345-dot <sampayorafael12345@gmail.com>
2026-04-15 01:18:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET || 'knfdQ0hM_uQFBr-DtL6K1-yAlRk'
 });
 
-// Middleware - INCREASED LIMITS for image uploads
+// Middleware
 app.use(cors());
 app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ limit: '200mb', extended: true }));
