6863b16bab648d2cd45a0e16b8e962331e722efd
sampayorafael12345-dot <sampayorafael12345@gmail.com>
2026-04-22 17:01:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function sendEmail(to, subject, htmlContent) {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'GLRA Realty <hello@glrarealty.com>',
+      from: 'GLRA Realty <hello@glrarealty.com>',  // ✅ Updated to your domain
       to: [to],
       subject: subject,
       html: htmlContent
